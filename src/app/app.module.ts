ecdf8fb4b08c834ab146cdc9625135748edea5aa
Git290234 <viktorkuenstler@googlemail.com>
2023-01-06 10:54:53 +0100

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {ProcessflowSimpleComponent} from './processflow-simple/processflow-simple.component';
-import {ProcessflowSimple2Component} from './processflow-simple2/processflow-simple2.component';
+import { ProcessflowSimpleComponent } from './processflow-simple/processflow-simple.component';
+import { ProcessflowSimple2Component } from './processflow-simple2/processflow-simple2.component';
+import { ProcessflowSimple3Component } from './processflow-simple3/processflow-simple3.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProcessflowSimpleComponent, ProcessflowSimple2Component ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ProcessflowSimpleComponent,
+    ProcessflowSimple2Component,
+    ProcessflowSimple3Component,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/processflow-simple3/processflow-simple3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-processflow-simple3',
+  templateUrl: './processflow-simple3.component.html',
+  styleUrls: ['./processflow-simple3.component.css']
+})
+export class ProcessflowSimple3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
